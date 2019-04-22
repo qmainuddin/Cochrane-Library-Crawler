83a885fb2f201ab42006374cce714605827204e6
mainuddin.talukdar <mainuddin.talukdar@tigerit.com>
2019-04-23 02:12:27 +0600

    added extra things

--- src/main/java/com/crawlers/core/Common.java
+++ src/main/java/com/crawlers/core/Common.java
@@ -1,5 +1,9 @@
 package com.crawlers.core;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
+
 public class Common {
     public static String buildStringFromParams(String... params){
         String paramStr = "";
@@ -7,5 +11,15 @@ public class Common {
             paramStr += param + " > ";
         }
         return paramStr.substring(0, paramStr.lastIndexOf(">")-2);
+    }
+    public static String dateProcessor(String date){
+        String[] partsOfDate = date.split(" ");
+        if(partsOfDate.length >= 3){
+            LocalDate localDate = LocalDate.of(Integer.parseInt(partsOfDate[2]), Month.valueOf(partsOfDate[1]), Integer.parseInt(partsOfDate[0]));
+            return localDate.toString();
+        }else {
+
+        }
+        return "";
     }
 }
